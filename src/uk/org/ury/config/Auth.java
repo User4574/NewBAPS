1873252f1ec11794092dcaedaed477afc0572da7
Nathan Lasseter <nathan.je.lasseter@googlemail.com>
2011-02-20 14:23:52 +0000

    Comment, comment, comment! Shout at me if i dont comment. I tend not to.

--- src/uk/org/ury/config/Auth.java
+++ src/uk/org/ury/config/Auth.java
@@ -2,16 +2,42 @@ package uk.org.ury.config;
 
 import uk.org.ury.database.UserClass;
 
+/**
+ * A login authorisation configuration
+ * 
+ * @author Nathan
+ */
 public class Auth {
 	
 	private String user;
 	private String pass;
 	private UserClass type;
 	
+	/**
+	 * Get the username of the login
+	 * 
+	 * @return String username
+	 */
 	public String getUser() { return user; }
+	/**
+	 * Get the password of the login
+	 * 
+	 * @return String password
+	 */
 	public String getPass() { return pass; }
+	/**
+	 * Get the access level of the login
+	 * 
+	 * @return UserClass type
+	 */
 	public UserClass getType() { return type; }
 	
+	/**
+	 * Create a login auth object
+	 * @param user The username for the login
+	 * @param pass The password for the login
+	 * @param type The access type for the login
+	 */
 	public Auth(String user, String pass, UserClass type) {
 		this.user = user;
 		this.pass = pass;
--- src/uk/org/ury/config/ConfigReader.java
+++ src/uk/org/ury/config/ConfigReader.java
@@ -10,24 +10,44 @@ import org.xml.sax.SAXParseException;
 
 import uk.org.ury.database.UserClass;
 
+/**
+ * Reads in an XML config file and creates config objects
+ * 
+ * @author Nathan
+ */
 public class ConfigReader {
 
 	private Database database;
 	private Auth auth;
 	
+	/**
+	 * Get the database configuration
+	 * 
+	 * @return Database database
+	 */
 	public Database getDatabase() { return database; }
+	/**
+	 * Get the login auth configuration
+	 * 
+	 * @return Auth auth
+	 */
 	public Auth getAuth() { return auth; }
 	
+	/**
+	 * Read in the config file and create the Database and Auth configuration objects.
+	 */
 	public ConfigReader() {
+		new ConfigReader("res/conf.xml");
+	}
+	
+	public ConfigReader(String configFile) {
 		
 		try {
 			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-			Document doc = docBuilder.parse (new File("res/conf.xml"));
+			Document doc = docBuilder.parse (new File(configFile));
 			doc.getDocumentElement().normalize();
 			
-			System.out.println(doc.getDocumentElement().getNodeName());
-
 			String user = doc.getElementsByTagName("user").item(0).getTextContent();
 			String pass = doc.getElementsByTagName("pass").item(0).getTextContent();
 			UserClass type;
